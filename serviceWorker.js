b6244c9726da23d7c895407db3a0e0ab056fda20
jono-openworld <53621272+jono-openworld@users.noreply.github.com>
2025-04-15 15:59:54 -0600

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,18 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  math_formulas.js"></script>
+  <script type="text/javascript" src="angles.js"></script>
+  <script src="
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
